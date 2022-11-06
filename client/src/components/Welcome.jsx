2056c1dbce97ed4e67c2b4540e15c8fdaa701127
DevSammy22 <sadedeji1992@yahoo.com>
2022-11-06 16:12:10 +0100

    initial commit

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,24 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+import { Loader } from "./";
+
+const Welcome = () => {
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> across the world...
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world. Buy and sell cryptocurrencies easily on
+            Krypt
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
