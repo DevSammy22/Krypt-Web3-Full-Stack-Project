e239595eebb9764ca96f6ceea4e8404c36640ae3
DevSammy22 <sadedeji1992@yahoo.com>
2022-11-07 23:17:31 +0100

    Written the smart contract for transactions

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 //Mora
 //idyt
 contract Transactions {
-    uint256 transactionsCounter;
+    uint256 transactionCount;
     event Transfer(
         address from,
         address receiver,
@@ -21,5 +21,46 @@ contract Transactions {
         string message;
         uint256 timeStamp;
         string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable receiver,
+        uint amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
     }
 }
