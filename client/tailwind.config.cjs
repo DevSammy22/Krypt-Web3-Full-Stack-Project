6922997d6e376305811fe61d87e3aa7ba421e7eb
DevSammy22 <sadedeji1992@yahoo.com>
2022-11-12 17:01:08 +0100

    Restructured the folder

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
+
   theme: {
     fontFamily: {
       display: ["Open Sans", "sans-serif"],
