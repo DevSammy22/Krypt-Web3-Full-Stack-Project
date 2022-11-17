44f3b696c40f4a11ef94cf46a6ab0deb4d418352
DevSammy22 <sadedeji1992@yahoo.com>
2022-11-17 02:39:05 +0100

    Updated

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -3,4 +3,9 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+// const tailwindcss = require("tailwindcss");
+// module.exports = {
+//   plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+// };
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  mode: "jit",
-  darkMode: false, // or 'media' or 'class'
+  //content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  // content: [
+  //   // Example content paths...
+  //   "./public/**/*.html",
+  //   "./src/**/*.{js,jsx,ts,tsx,vue}",
+  // ],
+  content: ["./public/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  //mode: "jit",
+  // darkMode: "class", // or 'media' or 'class'
 
   theme: {
     fontFamily: {
